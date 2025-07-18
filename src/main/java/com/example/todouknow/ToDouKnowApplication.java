47b0d16d2269886e2f9037ec48048ba884dabf91
raryett <raryett@outlook.com>
2025-07-17 23:59:06 -0300

    Commit inicial do projeto

---
+++ src/main/java/com/example/todouknow/ToDouKnowApplication.java
@@ -0,0 +1,13 @@
+package com.example.todouknow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDouKnowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDouKnowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/todouknow/ToDouKnowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todouknow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDouKnowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
